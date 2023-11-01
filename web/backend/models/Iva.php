c32232b33a0ed4ebc1941fac11b2e22076d06999
AndreDu95 <2220894@my.ipleiria.pt>
2023-11-01 00:27:09 +0000

    create CRUD Ivas
    GII generator

---
+++ web/backend/models/Iva.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ivas".
+ *
+ * @property int $id
+ * @property string|null $em_vigor
+ * @property string $descricao
+ * @property float $percentagem
+ */
+class Iva extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'ivas';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['em_vigor'], 'string'],
+            [['descricao', 'percentagem'], 'required'],
+            [['percentagem'], 'number'],
+            [['descricao'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'em_vigor' => 'Em Vigor',
+            'descricao' => 'Descricao',
+            'percentagem' => 'Percentagem',
+        ];
+    }
+}
