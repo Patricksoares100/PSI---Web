cc4943589e3e06007bcbb7a5523c996a4b51f0bb
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-11-05 12:39:18 +0000

    Finalização das tabelas
    adicionado as chaves estrangeiras nas tabelas, e adicionado o addForeignKey, assim não temos que adicionar as chaves estrangeiras no phpmyadmin sempre que fizer migrate fresh, vejam os comentarios que no manual n esta explicito e deu trabalho perceber

--- web/console/migrations/m231031_210433_criar_bd_inicial.php
+++ web/console/migrations/m231031_210433_criar_bd_inicial.php
@@ -34,19 +34,20 @@ class m231031_210433_criar_bd_inicial extends Migration
             'codigo_postal' => $this->string()->notNull(),
             'localidade' => $this->string()->notNull(),
         ]);
-        
-        $this->createTable('artigos',[
-            'id' =>$this->primaryKey(),
-            'nome' =>$this->string()->notNull(),
-            'descricao' =>$this->string()->notNull(),
-            'valor' =>$this->double()->notNull(),
-            'stock_atual' =>$this->integer()->notNull(),
-            'iva_id' =>$this->integer()->notNull(),
-            'fornecedor_id'=>$this->integer()->notNull(),
-            'categoria_id' =>$this->integer()->notNull(),
+
+        $this->createTable('artigos', [
+            'id' => $this->primaryKey(),
+            'nome' => $this->string()->notNull(),
+            'descricao' => $this->string()->notNull(),
+            'valor' => $this->double()->notNull(),
+            'stock_atual' => $this->integer()->notNull(),
+            'iva_id' => $this->integer()->notNull(),
+            'fornecedores_id' => $this->integer()->notNull(),
+            'categorias_id' => $this->integer()->notNull(),
+            'pessoas_id' => $this->integer()->notNull(),
         ]);
 
-        $this->createTable('fornecedores',[
+        $this->createTable('fornecedores', [
             'id' => $this->primaryKey(),
             'nome' => $this->string(),
             'telefone' => $this->integer(9),
@@ -59,40 +60,137 @@ class m231031_210433_criar_bd_inicial extends Migration
             'quantidade' => $this->integer()->notNull(),
             'valor' => $this->double()->notNull(),
             'valor_iva' => $this->double()->notNull(),
-            'artigo_id' =>$this->integer()->notNull(),
-            'carrinhocompras_id'=>$this->integer()->notNull(),
+            'artigos_id' => $this->integer()->notNull(),
+            'carrinhocompras_id' => $this->integer()->notNull(),
         ]);
 
         $this->createTable('ivas', [
-            'id' =>$this->primaryKey(),
-            'em_vigor' =>"ENUM('sim', 'nao')",
-            'descricao' =>$this->string()->notNull(),
-            'percentagem' =>$this->double()->notNull(),
+            'id' => $this->primaryKey(),
+            'em_vigor' => "ENUM('sim', 'nao')",
+            'descricao' => $this->string()->notNull(),
+            'percentagem' => $this->double()->notNull(),
         ]);
 
         $this->createTable('carrinho_compras', [
-            'id' =>$this->primaryKey(),
-            'data' =>$this->dateTime()->notNull(),
-            'valor_total' =>$this->double()->notNull(),
-            'iva_total' =>$this->integer()->notNull(),
-            'user_id' =>$this->integer()->notNull(),
-            'estado' =>"ENUM('activo', 'inactivo')",
+            'id' => $this->primaryKey(),
+            'data' => $this->dateTime()->notNull(),
+            'valor_total' => $this->double()->notNull(),
+            'iva_total' => $this->integer()->notNull(),
+            'pessoas_id' => $this->integer()->notNull(),
+            'estado' => "ENUM('activo', 'inactivo')",
         ]);
 
         $this->createTable('faturas', [
-            'id' =>$this->primaryKey(),
-            'data' =>$this->dateTime()->notNull(),
-            'valor_fatura' =>$this->double()->notNull(),
-            'estado' =>"ENUM('emitida', 'paga', 'cancelada')",
+            'id' => $this->primaryKey(),
+            'data' => $this->dateTime()->notNull(),
+            'valor_fatura' => $this->double()->notNull(),
+            'estado' => "ENUM('emitida', 'paga', 'cancelada')",
         ]);
 
         $this->createTable('avaliacaos', [
-            'id' =>$this->primaryKey(),
-            'comentario' =>$this->string()->notNull(),
-            'artigo_id' =>$this->integer()->notNull(),
-            'user_id' =>$this->integer()->notNull(),
-            'classificacao' =>"ENUM('1', '2', '3', '4', '5')",
+            'id' => $this->primaryKey(),
+            'comentario' => $this->string()->notNull(),
+            'artigos_id' => $this->integer()->notNull(),
+            'pessoas_id' => $this->integer()->notNull(),
+            'classificacao' => "ENUM('1', '2', '3', '4', '5')",
         ]);
+        //CHAVES ESTRANGEIRAS https://www.yiiframework.com/doc/guide/2.0/en/db-migrations
+        //atenção aos comentarios que não estao no manual do yii
+        // TEMOS QUE FAZER DROP DAS CHAVES? PERGUNTAR AO PROFESSOR
+
+        /*INICIO CHAVES ESTRANGEIRAS DA TABELA ARTIGOS */
+        $this->addForeignKey(
+            'fk-artigos-iva_id', // Nome da chave estrangeira (pode ser qualquer nome único)
+            'artigos', // Tabela da chave estrangeira(ou seja onde a chave estrangeira esta)
+            'iva_id', // Coluna da tabela 'artigos' que é a chave estrangeira
+            'ivas', // Tabela de referência
+            'id', // Coluna de referência na tabela 'ivas'
+            'CASCADE', // Ação a ser executada quando um registro relacionado na tabela 'ivas' é excluído
+            'CASCADE' // Ação a ser executada quando um registro relacionado na tabela 'ivas' é atualizado
+        );
+        $this->addForeignKey(
+            'fk-artigos-fornecedores_id',
+            'artigos',
+            'fornecedores_id',
+            'fornecedores',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-artigos-categoria_id',
+            'artigos',
+            'categorias_id',
+            'categorias',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        $this->addForeignKey(
+            'fk-artigos-pessoas_id',
+            'artigos',
+            'pessoas_id',
+            'pessoas',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        /*FIM CHAVES ESTRANGEIRAS DA TABELA ARTIGOS */
+
+        /*INICIO CHAVES ESTRANGEIRAS DA TABELA CARRINHO_ITEMS */
+
+        $this->addForeignKey(
+            'fk-carrinhoitems-artigos_id', 
+            'carrinho_items', 
+            'artigos_id', 
+            'artigos', 
+            'id', 
+            'CASCADE', 
+            'CASCADE' 
+        );
+        $this->addForeignKey(
+            'fk-carrinhoitems-carrinhocompras_id', 
+            'carrinho_items', 
+            'carrinhocompras_id', 
+            'carrinho_compras', 
+            'id', 
+            'CASCADE', 
+            'CASCADE' 
+        );
+        /*FIM CHAVES ESTRANGEIRAS DA TABELA CARRINHO_ITEMS */
+
+        /*INICIO CHAVES ESTRANGEIRAS DA TABELA  CARRINHO_COMPRAS*/
+        $this->addForeignKey(
+            'fk-carrinhocompras-pessoas_id',
+            'carrinho_compras',
+            'pessoas_id',
+            'pessoas',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        /*FIM CHAVES ESTRANGEIRAS DA TABELA  CARRINHO_COMPRAS*/
+
+        /*INICIO CHAVES ESTRANGEIRAS DA TABELA  avaliacaos*/
+        $this->addForeignKey(
+            'fk-avaliacaos-artigos_id', 
+            'avaliacaos', 
+            'artigos_id', 
+            'artigos', 
+            'id', 
+            'CASCADE', 
+            'CASCADE' 
+        );
+        $this->addForeignKey(
+            'fk-avaliacaos-pessoas_id',
+            'avaliacaos',
+            'pessoas_id',
+            'pessoas',
+            'id',
+            'CASCADE',
+            'CASCADE'
+        );
+        /*FIM CHAVES ESTRANGEIRAS DA TABELA  avaliacaos*/
     }
 
     /**
