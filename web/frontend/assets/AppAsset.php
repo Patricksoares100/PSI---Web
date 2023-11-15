66f13f294941d168c6c2b72e2d4bdf54b0004147
Patricksoares100 <2220905@my.ipleiria.pt>
2023-11-15 00:18:06 +0000

    Frontend - Template novo

--- web/frontend/assets/AppAsset.php
+++ web/frontend/assets/AppAsset.php
@@ -12,10 +12,9 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/style.css',
     ];
-    public $js = [
-    ];
+    public $js = [];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap5\BootstrapAsset',
