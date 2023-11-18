8b245286f298d0957903e094e7b943fc970599f1
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-11-18 15:27:55 +0000

    about e alterações no index

--- web/frontend/views/site/about.php
+++ web/frontend/views/site/about.php
@@ -2,6 +2,7 @@
 
 /** @var yii\web\View $this */
 
+use common\models\Empresa;
 use yii\helpers\Html;
 
 $this->title = 'About';
@@ -9,8 +10,13 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
+    <?php  $empresa = Empresa::find()->one()?>
+    <p>Nome: <?= $empresa->nome?> </p>
+    <p>Email: <?= $empresa->email?> </p>
+    <p>Contato: <?= $empresa->telefone?> </p>
+    <p>Número de Identificação Fiscal: <?= $empresa->nif?> </p>
+    <p>Morada: <?= $empresa->morada?> </p>
+    <p>Código Postal: <?= $empresa->codigo_postal?> </p>
+    <p>Localidade: <?= $empresa->localidade?> </p>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
 </div>
