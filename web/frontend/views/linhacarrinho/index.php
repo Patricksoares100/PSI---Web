2aefa875a43ce7a839a0ddf77225a91433329103
AndreDu95 <2220894@my.ipleiria.pt>
2023-11-26 21:18:43 +0000

    remove artigo do carrinho

--- web/frontend/views/linhacarrinho/index.php
+++ web/frontend/views/linhacarrinho/index.php
@@ -22,22 +22,22 @@ $this->params['breadcrumbs'][] = $this->title;
     </p>
 
     <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+    'dataProvider' => $dataProvider,
+    'columns' => [
+        ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
-            'quantidade',
-            'artigo_id',
-            'perfil_id',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, LinhaCarrinho $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id]);
-                }
-            ],
+        'id',
+        'quantidade',
+        'artigo_id',
+        'perfil_id',
+        [
+            'class' => ActionColumn::className(),
+            'urlCreator' => function ($action, LinhaCarrinho $model, $key, $index, $column) {
+                return Url::toRoute([$action, 'id' => $model->id]);
+            }
         ],
-    ]); ?>
+    ],
+]); ?>
 
 </div>-->
 
@@ -46,109 +46,120 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <body>
 
-    <!-- Cart Start -->
-    <div class="container-fluid">
-        <div class="row px-xl-5">
-            <div class="col-lg-8 table-responsive mb-5">
-                <table class="table table-light table-borderless table-hover text-center mb-0">
-                    <thead class="thead-dark">
-                        <tr>
-                            <th>Produto</th>
-                            <th>Preço</th>
-                            <th>% IVA</th>
-                            <th>Valor IVA</th>
-                            <th>Quantidade</th>
-                            <th>Total</th>
-                            <th>Remover</th>
-                        </tr>
-                    </thead>
-                    <?php foreach ($dataProvider->models as $model) : ?>
+<!-- Cart Start -->
+<div class="container-fluid">
+    <div class="row px-xl-5">
+        <div class="col-lg-8 table-responsive mb-5">
+            <table class="table table-light table-borderless table-hover text-center mb-0">
+                <thead class="thead-dark">
+                <tr>
+                    <th>Produto</th>
+                    <th>Preço</th>
+                    <th>% IVA</th>
+                    <th>Valor IVA</th>
+                    <th>Quantidade</th>
+                    <th>Total</th>
+                    <th>Remover</th>
+                </tr>
+                </thead>
+                <?php foreach ($dataProvider->models as $model) : ?>
                     <tbody class="align-middle">
-                        <tr>
-                            <td class="align-middle"><img src="img/product-1.jpg" alt="" style="width: 50px;"></td>
-                            <td class="align-middle"><?= $model->artigo->preco?> €</td> <!-- vai buscar ao modelo o getArtigo - MASTER DETAIL-->
-                            <td class="align-middle"><?= $model->artigo->iva->percentagem?> %</td>
-                            <td class="align-middle"><?= number_format(($model->artigo->iva->percentagem * $model->artigo->preco)/100,2)?> €</td>
-                            <td class="align-middle">
-                                <div class="input-group quantity mx-auto" style="width: 100px;">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-minus">
-                                            <i class="fa fa-minus"></i>
-                                        </button>
-                                    </div>
-                                    <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="<?= $model->quantidade ?>">
-                                    <div class="input-group-btn">
-                                        <button class="btn btn-sm btn-primary btn-plus">
-                                            <i class="fa fa-plus"></i>
-                                        </button>
-                                    </div>
+                    <tr>
+                        <td class="align-middle"><img src="img/product-1.jpg" alt="" style="width: 50px;"></td>
+                        <td class="align-middle"><?= $model->artigo->preco ?> €</td>
+                        <!-- vai buscar ao modelo o getArtigo - MASTER DETAIL-->
+                        <td class="align-middle"><?= $model->artigo->iva->percentagem ?> %</td>
+                        <td class="align-middle"><?= number_format(($model->artigo->iva->percentagem * $model->artigo->preco) / 100, 2) ?>
+                            €
+                        </td>
+                        <td class="align-middle">
+                            <div class="input-group quantity mx-auto" style="width: 100px;">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-sm btn-primary btn-minus">
+                                        <i class="fa fa-minus"></i>
+                                    </button>
                                 </div>
-                            </td>
-                            <td class="align-middle"><?=number_format($model->quantidade * ($model->artigo->preco + (($model->artigo->iva->percentagem * $model->artigo->preco)/100)),2)?> €</td>
-                            <td class="align-middle"><button class="btn btn-sm btn-danger"><i class="fa fa-times"></i></button></td>
-                        </tr>
+                                <input type="text"
+                                       class="form-control form-control-sm bg-secondary border-0 text-center"
+                                       value="<?= $model->quantidade ?>">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-sm btn-primary btn-plus">
+                                        <i class="fa fa-plus"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="align-middle"><?= number_format($model->quantidade * ($model->artigo->preco + (($model->artigo->iva->percentagem * $model->artigo->preco) / 100)), 2) ?>
+                            €
+                        </td>
+                        <td class="align-middle">
+                            <?= Html::a('X', ['/linhacarrinho/delete', 'id' => $model->id], ['class' => 'btn btn-sm btn-danger', 'data-method' => 'post']) ?>
+                        </td>
+                    </tr>
                     </tbody>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-            <div class="col-lg-4">
-                <h5 class="section-title position-relative text-uppercase mb-3"><span class="bg-secondary pr-3">Cart Summary</span></h5>
-                <div class="bg-light p-30 mb-5">
-                    <div class="border-bottom pb-2">
-                        <div class="d-flex justify-content-between mb-3">
-                            <h6>Total s/ IVA</h6>
+                <?php endforeach; ?>
+            </table>
+        </div>
+        <div class="col-lg-4">
+            <h5 class="section-title position-relative text-uppercase mb-3"><span
+                        class="bg-secondary pr-3">Cart Summary</span></h5>
+            <div class="bg-light p-30 mb-5">
+                <div class="border-bottom pb-2">
+                    <div class="d-flex justify-content-between mb-3">
+                        <h6>Total s/ IVA</h6>
+                        <?php
+                        $valorTotal = 0;
+                        foreach ($dataProvider->models as $model) : ?>
                             <?php
-                            $valorTotal=0;
-                            foreach ($dataProvider->models as $model) : ?>
-                                <?php
-                                $valorTotal += $model->quantidade * $model->artigo->preco;
-                                ?>
-                            <?php endforeach; ?>
-                            <h6><?=$valorTotal?></h6>
+                            $valorTotal += $model->quantidade * $model->artigo->preco;
+                            ?>
+                        <?php endforeach; ?>
+                        <h6><?= $valorTotal ?></h6>
 
-                        </div>
-                        <div class="d-flex justify-content-between">
-                            <h6 class="font-weight-medium">Valor IVA</h6>
+                    </div>
+                    <div class="d-flex justify-content-between">
+                        <h6 class="font-weight-medium">Valor IVA</h6>
+                        <?php
+                        $totalIvas = 0;
+                        foreach ($dataProvider->models as $model) : ?>
                             <?php
-                            $totalIvas=0;
-                            foreach ($dataProvider->models as $model) : ?>
-                                <?php
-                                $totalIvas += $model->quantidade * (($model->artigo->iva->percentagem * $model->artigo->preco)/100);
-                                ?>
-                            <?php endforeach; ?>
-                            <h6 class="font-weight-medium"><?=number_format($totalIvas, 2) ?></h6>
-                        </div>
+                            $totalIvas += $model->quantidade * (($model->artigo->iva->percentagem * $model->artigo->preco) / 100);
+                            ?>
+                        <?php endforeach; ?>
+                        <h6 class="font-weight-medium"><?= number_format($totalIvas, 2) ?></h6>
                     </div>
-                    <div class="pt-2">
-                        <div class="d-flex justify-content-between mt-2">
-                            <h5>Total</h5>
-                            <h5><?=number_format($totalIvas + $valorTotal, 2) ?></h5>
-                        </div>
-                        <a href="<?= \yii\helpers\Url::to(['/site/checkout']) ?>" class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</a>
+                </div>
+                <div class="pt-2">
+                    <div class="d-flex justify-content-between mt-2">
+                        <h5>Total</h5>
+                        <h5><?= number_format($totalIvas + $valorTotal, 2) ?></h5>
                     </div>
+                    <a href="<?= \yii\helpers\Url::to(['/site/checkout']) ?>"
+                       class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</a>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Cart End -->
+</div>
+<!-- Cart End -->
 
 
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+<!-- Back to Top -->
+<a href="#" class="btn btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
 
 
-    <!-- JavaScript Libraries -->
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+<!-- JavaScript Libraries -->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+<script src="lib/easing/easing.min.js"></script>
+<script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
-    <!-- Contact Javascript File -->
-    <script src="mail/jqBootstrapValidation.min.js"></script>
-    <script src="mail/contact.js"></script>
+<!-- Contact Javascript File -->
+<script src="mail/jqBootstrapValidation.min.js"></script>
+<script src="mail/contact.js"></script>
 
-    <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+<!-- Template Javascript -->
+<script src="js/main.js"></script>
 </body>
 
 </html>
