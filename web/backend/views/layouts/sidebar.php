0f97e54cb6dff73f52e986e1ccf56547dc36b867
Patricksoares100 <2220905@my.ipleiria.pt>
2023-12-03 00:18:35 +0000

    Imagem de empresa e perfil a ir para o sitio certo

--- web/backend/views/layouts/sidebar.php
+++ web/backend/views/layouts/sidebar.php
@@ -1,9 +1,17 @@
+<?php
+
+use common\models\Empresa;
+use common\models\Perfil;
+
+?>
+
+
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="http://brindeszorro-back.test/index.php?r=site%2Findex" class="brand-link">
+    <?php $empresa = Empresa::find()->one();?>
+    <a href="<?= Yii::$app->urlManager->createUrl(['empresa/view', 'id' => $empresa->id]) ?>" class="brand-link">
         <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-        <?php $empresa = \common\models\Empresa::find()->one();
-        if ($empresa){?>
+        <?php if ($empresa){?>
         <span class="brand-text font-weight-light"><?= $empresa->nome ?></span>
         <?php }else { ?>
         <span class="brand-text font-weight-light">Insira uma empresa</span>
@@ -13,12 +21,17 @@
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
+
+        <?php
+        $userId = Yii::$app->user->id;
+        $perfil = Perfil::find()->where(['id' => $userId])->one();
+        ?>
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="<?= $assetDir ?>/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <a href="http://brindeszorro-back.test/index.php?r=perfil%2Fview&id=<?= Yii::$app->user->identity->id?>" class="d-block"><?= Yii::$app->user->identity->username ?></a>
+                <a href="<?= Yii::$app->urlManager->createUrl(['perfil/view', 'id' => $perfil->id]) ?>" class="d-block"><?= Yii::$app->user->identity->username ?></a>
             </div>
         </div>
 
