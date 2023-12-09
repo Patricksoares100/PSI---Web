b48c033d06f272713e3d1c7ffa7eb654077e35e9
Patricksoares100 <2220905@my.ipleiria.pt>
2023-12-09 18:11:32 +0000

    Fix no carrinho vazio
    nao reconhecia o model quando o carrinho estava vazio ou era esvaziado

--- web/frontend/views/linhacarrinho/index.php
+++ web/frontend/views/linhacarrinho/index.php
@@ -136,8 +136,10 @@ $this->params['breadcrumbs'][] = $this->title;
                         <h5>Total</h5>
                         <h5><?= number_format($totalIvas + $valorTotal, 2) ?></h5>
                     </div>
-                    <a href="<?= \yii\helpers\Url::to(['/site/checkout','id' => $model->id]) ?>"
+                    <?php foreach ($dataProvider->models as $model) : ?>
+                    <a href="<?= Url::to(['/site/checkout','id' => $model->id]) ?>"
                        class="btn btn-block btn-primary font-weight-bold my-3 py-3">Proceed To Checkout</a>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </div>
