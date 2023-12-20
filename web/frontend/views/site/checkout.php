a086aec60ae6c78acd1503f588abc6d09152609c
Patricksoares100 <2220905@my.ipleiria.pt>
2023-12-20 21:33:18 +0000

    A mandar para o checkout sem criar linha de fatura
    Alterei para enviar para o checkout sem criar na base de dados. Reformulei o foreach para apenas apresentar artigos com artigo_id diferentes mas com o mesmo perfil_id

--- web/frontend/views/site/checkout.php
+++ web/frontend/views/site/checkout.php
@@ -51,31 +51,31 @@ $user = User::find()->where(['id' => $userId])->one();
                 <div class="row">
                     <div class="col-md-6 form-group">
                         <label>Primeiro Nome</label>
-                        <input class="form-control" type="text" value="<?= $perfil->nome?>" >
+                        <input class="form-control" type="text" value="<?= $perfil->nome ?>">
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Nif</label>
-                        <input class="form-control" type="text" value="<?=$perfil->nif?>">
+                        <input class="form-control" type="text" value="<?= $perfil->nif ?>">
                     </div>
                     <div class="col-md-6 form-group">
                         <label>E-mail</label>
-                        <input class="form-control" type="text" value="<?=$user->email?>">
+                        <input class="form-control" type="text" value="<?= $user->email ?>">
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Telefone</label>
-                        <input class="form-control" type="text" value="<?=$perfil->telefone?> ">
+                        <input class="form-control" type="text" value="<?= $perfil->telefone ?> ">
                     </div>
                     <div class="col-md-12 form-group">
                         <label>Endereço</label>
-                        <input class="form-control" type="text" value="<?=$perfil->morada?>">
+                        <input class="form-control" type="text" value="<?= $perfil->morada ?>">
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Localidade</label>
-                        <input class="form-control" type="text" value="<?=$perfil->localidade?>">
+                        <input class="form-control" type="text" value="<?= $perfil->localidade ?>">
                     </div>
                     <div class="col-md-6 form-group">
                         <label>Codigo Postal</label>
-                        <input class="form-control" type="text" value="<?=$perfil->codigo_postal ?>">
+                        <input class="form-control" type="text" value="<?= $perfil->codigo_postal ?>">
                     </div>
                 </div>
             </div>
@@ -87,72 +87,50 @@ $user = User::find()->where(['id' => $userId])->one();
             <div class="bg-light p-30 mb-5">
                 <div class="border-bottom">
                     <h6 class="mb-3">Products</h6>
-                    <?php
-                    foreach ($dataProvider->models as $model) {
-                        $artigos = $model->getArtigo()->all();
-                        foreach ($artigos as $artigo) {
+                </div>
+                <?php
+                $valorSemIva = 0;
+                $valorIva = 0;
+                foreach ($dataProvider->models as $model) {
+                    $artigos = $model->getArtigo()->all();
+                    foreach ($artigos as $artigo) {
+                        if ($model->perfil_id == $userId) {
                             ?>
                             <div class="d-flex justify-content-between">
                                 <p><?= $model->quantidade ?> - <?= $artigo->nome ?></p>
                                 <p><?= number_format(($model->quantidade * $artigo->preco), 2) ?></p>
                             </div>
-                        <?php }
-                    } ?>
+                            <?php
+                            $valorSemIva += $model->quantidade * $artigo->preco;
+                            $valorIva += $model->quantidade * (($artigo->iva->percentagem * $artigo->preco) / 100);
+                        }
+                    }
+                }
+                ?>
+                <div class="pt-2">
+                    <div class="d-flex justify-content-between mt-2">
+                        <h6>Total Iva</h6>
+                        <h6 class="font-weight-medium"><?= number_format($valorIva, 2) ?></h6>
+                    </div>
+                </div>
+                <div class="pt-2">
+                    <div class="d-flex justify-content-between mt-2">
+                        <h5>Total</h5>
+                        <h5><?= number_format($valorSemIva + $valorIva, 2) ?></h5>
+                    </div>
                 </div>
             </div>
-            <div class="border-bottom pt-3 pb-2">
-                <div class="d-flex justify-content-between mb-3">
-                    <h6>Valor s/iva</h6>
-                    <?php $valorSemIva = 0; ?>
-                    <?php foreach ($dataProvider->models as $model) {
-                        $valorSemIva += $model->quantidade * $model->artigo->preco;
-                    } ?>
-                    <h6><?= number_format($valorSemIva, 2) ?></h6>
-                </div>
-                <div class="d-flex justify-content-between">
-                    <h6 class="font-weight-medium">Valor Iva</h6>
-                    <?php $valorIva = 0; ?>
-                    <?php foreach ($dataProvider->models as $model) {
-                        $valorIva += $model->quantidade * (($model->artigo->iva->percentagem * $model->artigo->preco) / 100);
-                    } ?>
-                    <h6 class="font-weight-medium"><?= number_format($valorIva, 2) ?></h6>
-                </div>
-            </div>
-            <div class="pt-2">
-                <div class="d-flex justify-content-between mt-2">
-                    <h5>Total</h5>
-                    <h5><?= number_format($valorSemIva + $valorIva, 2) ?></h5>
+            <div class="mb-5">
+                <h5 class="section-title position-relative text-uppercase mb-3"><span
+                            class="bg-secondary pr-3">Payment</span></h5>
+                <div class="bg-light p-30">
+                    <a href="<?= Url::to(['fatura/index', 'id' => $model->id]) ?>"
+                       class="btn btn-block btn-primary font-weight-bold py-3">Fazer Pagamento</a>
                 </div>
             </div>
         </div>
-        <div class="mb-5">
-            <h5 class="section-title position-relative text-uppercase mb-3"><span
-                        class="bg-secondary pr-3">Payment</span></h5>
-            <div class="bg-light p-30">
-                <div class="form-group">
-                    <div class="custom-control custom-radio">
-                        <input type="radio" class="custom-control-input" name="payment" id="paypal">
-                        <label class="custom-control-label" for="paypal">Paypal</label>
-                    </div>
-                </div>
-                <div class="form-group">
-                    <div class="custom-control custom-radio">
-                        <input type="radio" class="custom-control-input" name="payment" id="directcheck">
-                        <label class="custom-control-label" for="directcheck">Direct Check</label>
-                    </div>
-                </div>
-                <div class="form-group mb-4">
-                    <div class="custom-control custom-radio">
-                        <input type="radio" class="custom-control-input" name="payment" id="banktransfer">
-                        <label class="custom-control-label" for="banktransfer">Bank Transfer</label>
-                    </div>
-                </div>
-                <a href="<?= Url::to(['fatura/index','id' => $model->id]) ?>" class="btn btn-block btn-primary font-weight-bold py-3">Fazer Pagamento</a>
-            </div>
-        </div>
-    </div>
 
-</div>
+    </div>
 </div>
 <!-- Checkout End -->
 </body>
