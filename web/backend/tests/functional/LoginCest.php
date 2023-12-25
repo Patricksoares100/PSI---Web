28b08586340246c9683770acd01a5e68b2ae0aa0
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-12-25 18:56:36 +0000

    teste login BO
    acham q faltam testar algo?

--- web/backend/tests/functional/LoginCest.php
+++ web/backend/tests/functional/LoginCest.php
@@ -33,6 +33,7 @@ class LoginCest
     public function loginUser(FunctionalTester $I)
     {
         $I->amOnRoute('/site/login');
+        $I->amGoingTo('Tentar entrar com os dados corretos');
         $I->fillField('Username', 'APR');
         $I->fillField('Password', 'password_0');
         $I->click('login-button');
@@ -41,4 +42,24 @@ class LoginCest
         $I->dontSeeLink('Login');
         $I->dontSeeLink('Signup');
     }
+    public function loginErrado(FunctionalTester $I)
+    {
+        $I->amOnRoute('/site/login');
+        $I->amGoingTo('Tentar entrar com os dados errados');
+        $I->fillField('LoginForm[username]', 'Vaifalhar');
+        $I->fillField('LoginForm[password]', '1234');
+        $I->click('Login');
+        $I->see('Incorrect username or password.');
+    }
+    public function loginEmBranco(FunctionalTester $I)
+    {
+        $I->amOnRoute('/site/login');
+        $I->amGoingTo('Tentar entrar sem com os dados em branco');
+        $I->fillField('LoginForm[username]', '');
+        $I->fillField('LoginForm[password]', '');
+        $I->click('Login');
+        $I->see('Username cannot be blank.');
+        $I->see('Password cannot be blank.');
+    }
+
 }
