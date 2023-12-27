254dc94ed9530adb6d51047d4455291916c13e1e
Patricksoares100 <2220905@my.ipleiria.pt>
2023-12-27 22:09:12 +0000

    Teste Homecest - Front
    o H5 é a formatação de titulo que costumamos usar no html , o <h5></5>

--- web/frontend/tests/functional/HomeCest.php
+++ web/frontend/tests/functional/HomeCest.php
@@ -10,8 +10,8 @@ class HomeCest
     {
         $I->amOnRoute(\Yii::$app->homeUrl);
         $I->see('Home');
-        $I->seeLink('About');
-        $I->click('About');
-        $I->see('About', 'h1');
+        $I->seeLink('Home');
+        $I->click('Home');
+        $I->see('Quality Product', 'h5');
     }
 }
