d06983fc1aec952906c4b6f42f6b15ed1f066942
AndreDu95 <2220894@my.ipleiria.pt>
2023-12-29 00:47:07 +0000

    Testes unitarios das Avaliações e Faturas
    os testes estão iguais ao do artigo como foi feito no teams, apenas da o erro do grabRecord.
    
    2 testes frontend.
    + 2 no backend das categorias e artigos

--- web/frontend/tests/unit/AvaliacaoTest.php
+++ web/frontend/tests/unit/AvaliacaoTest.php
@@ -1,7 +1,6 @@
 <?php
 
-
-namespace frontend\tests\Unit;
+namespace frontend\tests\unit;
 
 use frontend\tests\UnitTester;
 use common\models\Avaliacao;
@@ -22,10 +21,55 @@ class AvaliacaoTest extends \Codeception\Test\Unit
         $avaliacao->comentario = '';
         $this->assertFalse($avaliacao->validate(['comentario']));
 
-        $avaliacao->artigo_id = '';
+        $avaliacao->artigo_id = null;
         $this->assertFalse($avaliacao->validate(['artigo_id']));
 
-        $avaliacao->perfil_id = '';
+        $avaliacao->perfil_id = null;
         $this->assertFalse($avaliacao->validate(['perfil_id']));
+    }
+
+    public function testDadosInvalidos()
+    {
+        $avaliacao = new Avaliacao();
+
+        // Preencher campos com dados inválidos
+        $avaliacao->comentario = 123;
+        $avaliacao->artigo_id = 'abc';
+        $avaliacao->perfil_id = 'def';
+
+        // Verificar se é inválido
+        $this->assertFalse($avaliacao->validate(['comentario']));
+        $this->assertFalse($avaliacao->validate(['artigo_id']));
+        $this->assertFalse($avaliacao->validate(['perfil_id']));
+    }
+
+    public function testGuardarAvaliacaoValida()
+    {
+        //alinea B C ficha 7
+        $avaliacao = new Avaliacao();
+
+        $avaliacao->comentario = 'Bom Produto';
+        $avaliacao->artigo_id = 1;
+        $avaliacao->perfil_id = 2;
+
+        $avaliacao->save();
+
+        $this->tester->seeRecord('common\models\Avaliacao', ['comentario' => 'Bom Produto', 'artigo_id' => 1, 'perfil_id' => 2]);
+    //}
+
+    //public function testUpdateAvaliacao(){
+        // alineas D E F e G FIcha7
+        //$old_avaliacao = $this->tester->grabRecord('common\models\Avaliacao', ['comentario' => 'Bom Produto', 'artigo_id' => 1, 'perfil_id' => 2]);
+
+        $avaliacao->comentario = 'Podia ser Melhor';
+        $avaliacao->artigo_id = 2;
+        $avaliacao->perfil_id = 3;
+
+        $avaliacao->save();
+        $this->tester->dontSeeRecord('common\models\Avaliacao', ['comentario' => 'Bom Produto', 'artigo_id' => 1, 'perfil_id' => 2]);
+        $this->tester->seeRecord('common\models\Avaliacao', ['comentario' => 'Podia ser Melhor', 'artigo_id' => 2, 'perfil_id' => 3]);
+
+        $avaliacao->delete();
+        $this->tester->dontSeeRecord('common\models\Avaliacao', ['comentario' => 'Podia ser Melhor', 'artigo_id' => 2, 'perfil_id' => 3]);
     }
 }
--- web/frontend/tests/unit/FaturaTest.php
+++ web/frontend/tests/unit/FaturaTest.php
@@ -1,7 +1,6 @@
 <?php
 
-
-namespace frontend\tests\Unit;
+namespace frontend\tests\unit;
 
 use common\models\Fatura;
 use frontend\tests\UnitTester;
@@ -28,5 +27,54 @@ class FaturaTest extends \Codeception\Test\Unit
 
         $fatura->perfil_id = null;
         $this->assertFalse($fatura->validate(['perfil_id']));
+    }
+
+    public function testDadosInvalidos()
+    {
+        $fatura = new Fatura();
+
+        // Preencher campos com dados inválidos
+        $fatura->data = null;
+        $fatura->valor_fatura = 'abc';
+        $fatura->estado = 10;
+        $fatura->perfil_id = 'def';
+
+        // Verificar se é inválido
+        $this->assertFalse($fatura->validate(['data']));
+        $this->assertFalse($fatura->validate(['valor_fatura']));
+        $this->assertFalse($fatura->validate(['estado']));
+        $this->assertFalse($fatura->validate(['perfil_id']));
+    }
+
+    public function testGuardarFaturaValida()
+    {
+        //alinea B C ficha 7
+        $fatura = new Fatura();
+
+        $fatura->data = '2023-06-30 18:45:10';
+        $fatura->valor_fatura = 18.99;
+        $fatura->estado = 'Emitida';
+        $fatura->perfil_id = 2;
+
+        $fatura->save();
+
+        $this->tester->seeRecord('common\models\Fatura', ['data' => '2023-06-30 18:45:10', 'valor_fatura' => 18.99, 'estado' => 'Emitida', 'perfil_id' => 2]);
+    //}
+
+        //public function testUpdateFatura(){
+        // alineas D E F e G FIcha7
+        //$old_$fatura = $this->tester->grabRecord('common\models\Fatura', ('common\models\Fatura', ['data' => '2023-06-30 18:45:10', 'valor_fatura' => 18.99, 'estado' => 'Emitida', 'perfil_id' => 2]);
+
+        $fatura->data = '2023-10-05 11:45:10';
+        $fatura->valor_fatura = 189.99;
+        $fatura->estado = 'Paga';
+        $fatura->perfil_id = 3;
+
+        $fatura->save();
+        $this->tester->dontSeeRecord('common\models\Fatura', ['data' => '2023-06-30 18:45:10', 'valor_fatura' => 18.99, 'estado' => 'Emitida', 'perfil_id' => 2]);
+        $this->tester->seeRecord('common\models\Fatura', ['data' => '2023-10-05 11:45:10', 'valor_fatura' => 189.99, 'estado' => 'Paga', 'perfil_id' => 3]);
+
+        $fatura->delete();
+        $this->tester->dontSeeRecord('common\models\Fatura', ['data' => '2023-10-05 11:45:10', 'valor_fatura' => 189.99, 'estado' => 'Paga', 'perfil_id' => 3]);
     }
 }
