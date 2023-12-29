e744cfeb818321c1705e4c3520340ebb07bb71c7
AndreDu95 <2220894@my.ipleiria.pt>
2023-12-29 19:55:49 +0000

    FavoritoCest FO
    Não está a funcionar, fica só no commit para ficar acessivel a todos

---
+++ web/frontend/tests/functional/FavoritoCest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace frontend\tests\functional;
+
+use common\fixtures\UserFixture;
+use common\models\Perfil;
+use common\models\User;
+use frontend\tests\FunctionalTester;
+
+class FavoritoCest
+{
+    public function _fixtures()
+    {
+        return [
+            'user' => [
+                'class' => UserFixture::class,
+                'dataFile' => codecept_data_dir() . 'login_data.php'
+            ]
+        ];
+    }
+
+
+    public function _before(FunctionalTester $I)
+    {
+        $user = new User();
+        $auth = \Yii::$app->authManager;
+        $user->username = "visitante";
+        $user->email = "visitante@a.a";
+        $user->setPassword("teste123");
+        $user->generateAuthKey();
+        $user->generateEmailVerificationToken();
+        $user->save();
+        $auth->assign($auth->getRole('cliente'), $user->getId());
+        $perfil = new Perfil();
+        $perfil->nome = "Visitante";
+        $perfil->codigo_postal = "2000-123";
+        $perfil->telefone = "912312312";
+        $perfil->nif = "987987564";
+        $perfil->morada = "rua xpto";
+        $perfil->localidade = 'Porto';
+        $perfil->save();
+
+        $I->amOnRoute('/site/login');
+
+    }
+
+    //Favoritos não é acessivel por pessoas não autenticadas
+    public function verificarNaoAutenticado(FunctionalTester $I)
+    {
+        $I->click('a[id="favorito"]');
+        $I->see('My Login');
+    }
+
+    //Verificar se esta acessivel depois de autenticado
+    public function verificarAutenticado(FunctionalTester $I)
+    {
+        $I->see('Login');
+        $I->fillField('Username', 'cliente');
+        $I->fillField('Password', 'teste123');
+        $I->click('login-button');
+        $I->see('Logout');
+        $I->click('a[id="favorito"]');
+        $I->see('Favoritos');
+    }
+
+    public function adicionarArtigoFavoritosSemLogin(FunctionalTester $I)
+    {
+        $I->amOnRoute('artigo/detail?id=1');
+        $I->see('Caneta Aluminio');
+        $I->seeLink('Add Carrinho');
+        $I->click('Add Favoritos');
+
+        $I->see('My Login');
+    }
+
+    public function adicionarArtigoFavoritosComLogin(FunctionalTester $I)
+    {
+        $I->amOnRoute('artigo/detail?id=1');
+        $I->see('Caneta Aluminio');
+        $I->seeLink('Add Carrinho');
+        $I->click('Add Favoritos');
+
+        $I->see('Carrinho');
+    }
+}
