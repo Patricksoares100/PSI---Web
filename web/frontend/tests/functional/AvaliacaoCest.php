3150d00d3099346e29bee7dacd2ee14dc0e5ebb7
Patricksoares100 <2220905@my.ipleiria.pt>
2023-12-30 01:36:23 +0000

    Division - Functional War

---
+++ web/frontend/tests/functional/AvaliacaoCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace frontend\tests\Functional;
+
+use frontend\tests\FunctionalTester;
+
+class AvaliacaoCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
---
+++ web/frontend/tests/functional/FaturaCest.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace frontend\tests\Functional;
+
+use frontend\tests\FunctionalTester;
+
+class FaturaCest
+{
+    public function _before(FunctionalTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
