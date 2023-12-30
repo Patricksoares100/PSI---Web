d60c834fccd89c9bba011b77caeb39ab964ad1f7
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2023-12-30 12:37:06 +0000

    Teste de aceitação a funcionar, agora é esperar pelo ultimo funcional
    e começar a construir o teste

--- web/frontend/tests/acceptance/HomeCest.php
+++ web/frontend/tests/acceptance/HomeCest.php
@@ -9,13 +9,14 @@ class HomeCest
 {
     public function checkHome(AcceptanceTester $I)
     {
-        $I->amOnRoute(Url::toRoute('/site/index'));
-        $I->see('My Application');
+        //$I->amOnRoute(Url::toRoute('/site/index'));
+        $I->amOnPage('/');
+        $I->see('Login');
 
         $I->seeLink('About');
         $I->click('About');
         $I->wait(2); // wait for page to be opened
 
-        $I->see('This is the About page.');
+        $I->see('QUEM SOMOS');
     }
 }
