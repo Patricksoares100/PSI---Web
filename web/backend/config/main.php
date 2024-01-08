df555227abf4cdc4ff969238f6ee2892cfe66482
RafaelMCoelho92 <114529694+RafaelMCoelho92@users.noreply.github.com>
2024-01-08 22:43:58 +0000

    pronto pra entrega  de web

--- web/backend/config/main.php
+++ web/backend/config/main.php
@@ -20,10 +20,10 @@ return [
         'request' => [
             'csrfParam' => '_csrf-backend',
             'parsers' => [
-                'application/json' => 'yii\web\JsonParser',],
+                'application/json' => 'yii\web\JsonParser',]/*,
                 'acceptableContentTypes' => [
                     'application/json' => 1,
-                ],
+                ],*/
         ],
         'user' => [
             'identityClass' => 'common\models\User',
