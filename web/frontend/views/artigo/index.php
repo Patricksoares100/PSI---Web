3b73e65ee399c9e951db97cf882cf5e975491263
Patricksoares100 <2220905@my.ipleiria.pt>
2024-01-26 12:56:29 +0000

    imagens artigos com escala certa

--- web/frontend/views/artigo/index.php
+++ web/frontend/views/artigo/index.php
@@ -25,7 +25,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         <div class="col-lg-4 col-md-6 col-sm-6 pb-1">
                             <div class="product-item bg-light mb-4">
                                 <div class="product-img position-relative overflow-hidden">
-                                    <img class="img-fluid w-100" src="<?=Yii::$app->params['caminhoBackend']. '/'. $model->imagens[0]->image_path?>" style="width: 5cm; height: 5cm;"  alt="Imagem do artigo">
+                                    <img class="img-fluid w-100" src="<?=Yii::$app->params['caminhoBackend']. '/'. $model->imagens[0]->image_path?>" style="width: 5cm; height: 5cm; object-fit: contain;" alt="Imagem do artigo">
                                     <div class="product-action">
                                         <?php if ($model->stock_atual > 0): ?>
                                         <a class="btn btn-outline-dark btn-square" href="<?= Url::to(['linhacarrinho/create', 'id' => $model->id])?>"><i class="fa fa-shopping-cart"></i></a>
